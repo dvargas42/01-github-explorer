0155653a76541afe2c4f44528bf6d52cf9885ad8
dvargas42 <dvargas.eng@gmail.com>
2022-02-19 10:45:58 -0800

    refactor: change code style

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,32 +1,31 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-import { RepositoryItem } from "./RepositoryItem"
+import { RepositoryItem } from "./RepositoryItem";
 
-import '../styles/repositories.scss'
+import "../styles/repositories.scss";
 
 function RepositoryList() {
-  const [repositories, setRepositories] = useState([])
+  const [repositories, setRepositories] = useState([]);
 
   useEffect(() => {
     axios({
       method: "get",
-      url: "https://api.github.com/orgs/rocketseat/repos"
-    }).then(response => setRepositories(response.data))
-  }, [])
+      url: "https://api.github.com/orgs/rocketseat/repos",
+    }).then((response) => setRepositories(response.data));
+  }, []);
 
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        {repositories.map(repository => (
+        {repositories.map((repository) => (
           <RepositoryItem key={repository.name} repository={repository} />
         ))}
       </ul>
-
     </section>
-  )
+  );
 }
 
-export { RepositoryList }
+export { RepositoryList };
