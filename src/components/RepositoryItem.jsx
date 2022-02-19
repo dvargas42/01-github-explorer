863f06dfe0fdaa32e4d1c139bf4e4dfa11586d7b
dvargas42 <dvargas.eng@gmail.com>
2022-02-19 10:58:23 -0800

    refactor: implement code style

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,14 +1,12 @@
 function RepositoryItem({ repository }) {
   return (
     <li>
-      <strong>{ repository?.full_name?? 'Default'}</strong>
+      <strong>{repository?.full_name ?? "Default"}</strong>
       <p>{repository?.description}</p>
 
-      <a href={repository?.html_url}>
-        Acessar repositório
-      </a>
+      <a href={repository?.html_url}>Acessar repositório</a>
     </li>
-  )
+  );
 }
 
 export { RepositoryItem };
